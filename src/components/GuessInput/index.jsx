eb869251c96a12f4056f98cb6874ce1606caf261
Abdullah Mzaien <xa300600@hotmail.com>
2023-05-08 23:41:50 +0300

    first exercise

---
+++ src/components/GuessInput/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function GuessInput() {
+  const [guess, setGuess] = useState("");
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log({ guess });
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        type="text"
+        id="guess-input"
+        name="guess-input"
+        value={guess}
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="\w{5}"
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      ></input>
+    </form>
+  );
+}
